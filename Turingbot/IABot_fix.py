6583476d6c8a30168b33934585d874d49e182683
MojtabaKD <mkbox86@gmail.com>
2021-12-22 19:28:03 +0330

    Rewritten the code
    Logical algorithm is the same, but used wikitextparser according to the Huji's suggestion + used flake8 to correct the appearance of the code. Tested it, works good.

--- Turingbot/IABot_fix.py
+++ Turingbot/IABot_fix.py
@@ -1,146 +1,56 @@
 #!/usr/bin/python
-#"صفحه‌های دارای ارجاع با متغیر تکراری" category has 14,358 members now! 21 december 2021 2:20 PM
-#released under MIT License (which I don't know it exactly yet!)
+
+# released under MIT License
+# This code is executed by user:Turingbot which belongs
+# to the user:Mojtabakd on fawiki
+# Upgraded according to user:Huji suggestions:
+# https://fa.wikipedia.org/w/index.php?title=&oldid=33780764
 
 import pywikibot
-import re
-import requests
+from pywikibot import pagegenerators
+import wikitextparser as wtp
 
-S = requests.Session()
+cat = pywikibot.Category(pywikibot.Site("fa"),
+                         "رده:صفحه‌های دارای ارجاع با متغیر تکراری")
+gen = pagegenerators.CategorizedPageGenerator(cat)
+pgen = pagegenerators.PreloadingGenerator(gen)
 
-URL = "https://fa.wikipedia.org/w/api.php"
+site = pywikibot.Site('fa', 'wikipedia')
 
-PARAMS = {
-    "action": "query",
-    "cmtitle": "رده:صفحه‌های دارای ارجاع با متغیر تکراری",
-    "cmlimit": "200",
-    "list": "categorymembers",
-    "format": "json"
-}
+edit_limit = 10
+edit_counter = 0
 
-R = S.get(url=URL, params=PARAMS)
-DATA = R.json()
+for curr_page in pgen:
+    parsed = wtp.parse(curr_page.text)
+    print(str(curr_page.title()))
 
-PAGES = DATA['query']['categorymembers']
+    old_templates = []
+    new_templates = []
 
-for curr_page in PAGES:
-    print(curr_page['title'])
-    site = pywikibot.Site('fa', 'wikipedia')
-    page = pywikibot.Page(site, curr_page['title'].encode().decode())
-    text = page.get()
+    for template in parsed.templates:
+        if (template.name.strip() == 'یادکرد وب'):
+            args_names = []
 
+            for arg in template.arguments:
+                args_names.append(arg.name.strip())
 
-    citeweb = []
+            if ((('archiveurl' in args_names) or
+                    ('archive-url' in args_names)) and
+                    ('پیوند بایگانی' in args_names) and
+                    ('تاریخ بایگانی' in args_names)):
+                old_templates.append(str(template))
+                template.del_arg('پیوند بایگانی')
+                template.del_arg('تاریخ بایگانی')
+                new_templates.append(str(template))
 
-    ind = []
+    tmp = curr_page.text
 
-    for match in re.finditer(r'(\{\{یادکرد وب)([^\}\{]*?)(\|\s*archive[-]?url\s*=\s*)([^\s\|\}]*)([^\}\{]*?)(\|\s*تاریخ بایگانی\s*=\s*)([^\|\}]*)([^\}\{]*?)(\|\s*پیوند بایگانی\s*=\s*)([^\s\|\}]*)([^\}\{]*?\}\})', text):
-        ind.append((match.start(), match.end(), match.span(6), match.span(7), match.span(9), match.span(10)))
-    
-    if len(ind)>0:
-        tmp = text[0:ind[0][0]]
-        i = 0
-        
-        for x in ind:
-            if (i<len(ind)-1):
-                tmp += text[x[0]:x[2][0]] + text[x[3][1]:x[4][0]] + text[x[5][1]:x[1]] + text[x[1]:ind[i+1][0]]
-            else:
-                tmp += text[x[0]:x[2][0]] + text[x[3][1]:x[4][0]] + text[x[5][1]:x[1]] + text[x[1]:]
-            i += 1
-        # print(tmp)
-        page.put(tmp, "حذف پارامتر تکراری در الگوی «یادکرد وب»")
+    for i in range(len(old_templates)):
+        tmp = tmp.replace(old_templates[i], new_templates[i])
 
-#This is a version of above code for tests in the sandbox:
+    if (tmp != curr_page.text):
+        edit_counter += 1
+        curr_page.put(tmp, "حذف پارامتر تکراری در الگوی «یادکرد وب»")
 
-# site = pywikibot.Site('fa', 'wikipedia')
-# page = pywikibot.Page(site, 'کاربر:Mojtabakd/صفحه تمرین')
-# text = page.get()
-
-# citeweb = []
-
-# ind = []
-
-# # print("******New Test*******")
-# # print("*********************")
-# # print("*********************")
-# # print("*********************")
-# # print("*********************")
-# # print("*********************")
-
-# for match in re.finditer(r'(\{\{یادکرد وب)([^\}\{]*?)(\|\s*archive[-]?url\s*=\s*)([^\s\|\}]*)([^\}\{]*?)(\|\s*تاریخ بایگانی\s*=\s*)([^\|\}]*)([^\}\{]*?)(\|\s*پیوند بایگانی\s*=\s*)([^\s\|\}]*)([^\}\{]*?\}\})', text):
-    # # print(str(match)+"+++++++")
-    
-    # # try:
-        # # print(str(match.group(1))+"------1-")
-    # # except IndexError:
-        # # print("1 doesn't exist-------")
-    
-    # # try:
-        # # print(str(match.group(2))+"------2-")
-    # # except IndexError:
-        # # print("2 doesn't exist-------")
-    
-    # # try:
-        # # print(str(match.group(3))+"------3-")
-    # # except IndexError:
-        # # print("3 doesn't exist-------")
-    
-    # # try:
-        # # print(str(match.group(4))+"------4-")
-    # # except IndexError:
-        # # print("4 doesn't exist-------")
-    
-    # # try:
-        # # print(str(match.group(5))+"------5-")
-    # # except IndexError:
-        # # print("5 doesn't exist-------")
-    
-    # # try:
-        # # print(str(match.group(6))+"------6-")
-    # # except IndexError:
-        # # print("6 doesn't exist-------")
-    
-    # # try:
-        # # print(str(match.group(7))+"------7-")
-    # # except IndexError:
-        # # print("7 doesn't exist-------")
-    
-    # # try:
-        # # print(str(match.group(8))+"------8-")
-    # # except IndexError:
-        # # print("8 doesn't exist-------")
-    
-    # # try:
-        # # print(str(match.group(9))+"------9-")
-    # # except IndexError:
-        # # print("9 doesn't exist-------")
-    
-    # # try:
-        # # print(str(match.group(10))+"-----10-")
-    # # except IndexError:
-        # # print("10 doesn't exist-------")
-    
-    # # try:
-        # # print(str(match.group(11))+"-----11-")
-    # # except IndexError:
-        # # print("11 doesn't exist-------")
-    
-    # # print("^^^^^^^")
-    # # print(len(match.groups()))
-    # # print("^^^^^^^")
-    
-    # ind.append((match.start(), match.end(), match.span(6), match.span(7), match.span(9), match.span(10)))
-    
-
-# if len(ind)>0:
-    # tmp = text[0:ind[0][0]]
-    # i = 0
-    
-    # for x in ind:
-        # if (i<len(ind)-1):
-            # tmp += text[x[0]:x[2][0]] + text[x[3][1]:x[4][0]] + text[x[5][1]:x[1]] + text[x[1]:ind[i+1][0]]
-        # else:
-            # tmp += text[x[0]:x[2][0]] + text[x[3][1]:x[4][0]] + text[x[5][1]:x[1]] + text[x[1]:]
-        # i += 1
-    # # print(tmp)
-    # page.put(tmp, "تست حذف پارامتر تکراری در «یادکرد ویکی»")
+    if (edit_counter >= edit_limit):
+        break
