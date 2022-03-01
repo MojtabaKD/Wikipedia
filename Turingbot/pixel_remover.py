c7ed564be82a3794e1db7091bde3da5a572f75e0
MojtabaKD <mkbox86@outlook.com>
2022-03-01 15:51:36 +0330

    Removing wrong pixel parameter
    A snippet code to remove wrong pixel parameters in the infobox of some category

---
+++ Turingbot/pixel_remover.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+# released under MIT License
+# This code is executed by user:Turingbot which belongs
+# to the user:Mojtabakd on fawiki
+# Upgraded according to user:Huji suggestions:
+# https://fa.wikipedia.org/w/index.php?title=&oldid=33780764
+
+import pywikibot
+from pywikibot import pagegenerators
+import wikitextparser as wtp
+import re
+
+cat = pywikibot.Category(pywikibot.Site("fa"),
+                         "رده:صفحه‌هایی که از جعبه اطلاعات شاعر"
+                         + " و نویسنده با پارامترهای نامعلوم استفاده می‌کنند")
+gen = pagegenerators.CategorizedPageGenerator(cat)
+pgen = pagegenerators.PreloadingGenerator(gen)
+
+site = pywikibot.Site('fa', 'wikipedia')
+
+# pgen = []
+# pgen.append(pywikibot.Page(site, 'کاربر:Mojtabakd/صفحه تمرین'))
+
+edit_limit = 10
+edit_counter = 0
+
+try:
+    for curr_page in pgen:
+        parsed = wtp.parse(curr_page.text)
+        print(str(curr_page.title()))
+
+        modified_text = ""
+
+        for line in curr_page.text.splitlines():
+            x = re.search(r"(.*?)(\|[\d]+?px)(.*?)", line)
+            if x and not re.search(r"[\[]", x.group(1)):
+                print(x.group(2))
+                modified_line = x.group(1)+x.group(3)
+                modified_text += modified_line + "\r\n"
+            else:
+                modified_text = modified_text + line + "\r\n"
+
+        if (modified_text != curr_page.text):
+            try:
+                edit_counter += 1
+                curr_page.put(modified_text, "اصلاح پارامتر پیکسل اشتباه"
+                                             + " در تصویر جعبه اطلاعات")
+                # print(modified_text)
+            except pywikibot.exceptions.LockedPageError:
+                print(curr_page.title()+" is locked!")
+
+        if (edit_counter >= edit_limit):
+            break
+except Exception as e:
+    print("Error: {0}".format(e))
