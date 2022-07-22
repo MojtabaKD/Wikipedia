ccfe2f34ce4a11a52ced029bc6b0cfb9a6b1d768
MojtabaKD <mkbox86@outlook.com>
2022-07-22 20:40:13 +0430

    Create Cat-Updater.py

---
+++ Turingbot/Cat-Updater.py
@@ -0,0 +1,154 @@
+#!/usr/bin/python
+
+# released under MIT License
+# This code is executed by user:Turingbot which belongs
+# to the user:Mojtabakd on fawiki
+# It updates categories in a list of pages given by a file
+
+import pywikibot
+import wikitextparser as wtp
+import re
+
+update_list = open('test.txt', 'r')
+upd_lines = update_list.readlines()
+
+# exclude_list = open('exclude_list.txt', 'r')
+# exc_lines = exclude_list.readlines()
+
+pgen = []
+
+site_fa = pywikibot.Site('fa', 'wikipedia')
+site_en = pywikibot.Site('en', 'wikipedia')
+
+for line in upd_lines:
+    pgen.append(pywikibot.Page(site_fa, line.strip()))
+
+# pgen = []
+# pgen.append(pywikibot.Page(site_fa, 'کاربر:Mojtabakd/صفحه تمرین'))
+
+edit_limit = 4000
+edit_counter = 0
+
+counter = 0
+
+out = open("out.txt", "a")
+
+# pywikibot's categories() isn't helpful here, becuase
+# it returns extra unneeded categories. So we define:
+def GetCats(src, lang):
+    if lang=="en":
+        cat = "category"
+    elif lang=="fa":
+        cat = "رده"
+    
+    pattern = r'\[\[[^:\[\]]*?' + cat + r'[^:\[\]]*?:[^:\[\]]*?\]\]'
+    cats = re.findall(pattern, src, flags=re.I)
+
+    stripped = []
+    
+    for x in cats:
+        tmp = re.sub(r"\[\[", "", x)
+        tmp = re.sub(r"\]\]", "", tmp)
+        tmp = re.sub(r"\|.*", "", tmp)
+        stripped.append(tmp)
+
+    stripped.sort()
+    
+    for x in stripped:
+        print('x='+x)
+    
+    return stripped
+
+for curr_page in pgen:
+    eng_cats = []
+    loc_cats = []
+    eng_cats_trans = []
+    no_fa_exists = []
+    fa_cats_final = []
+    if curr_page.exists():
+        print("="*10+curr_page.title()+"="*10)
+        
+        page_is_conn = False
+
+        try:
+            curr_page_item = pywikibot.ItemPage.fromPage(curr_page)
+            engtitle = curr_page_item.getSitelink('enwiki')
+            page_is_conn = True
+        except Exception as e:
+            print('Page is not connected.')
+
+        if page_is_conn:
+            # So the page is connected we can continue the task
+            engpage = pywikibot.Page(site_en, engtitle)
+            for cat_title in GetCats(engpage.text, 'en'):
+                cat = pywikibot.Category(site_en, cat_title)
+                if cat.exists():
+                    eng_cats.append(cat.title())
+                else:
+                    print("#"*10+cat.title()+"#"*10)
+                    print('Eng cat doesn\'t exists')
+            
+            # Now fetch the corresponding fawiki titles
+            # of the english categories of the page if ...
+            for x in eng_cats:
+                try:
+                    x_page = pywikibot.Page(site_en, x)
+                    x_item = pywikibot.ItemPage.fromPage(x_page)
+                    fatitle = x_item.getSitelink('fawiki')
+                    eng_cats_trans.append(fatitle)
+                except Exception as e:
+                    print("The \"" + x + "\" is not created in fawiki.")
+
+            # now we determine which categories of fawiki page
+            # are local to make an exception for them while updating
+            for cat_title in GetCats(curr_page.text, 'fa'):
+                cat = pywikibot.Category(site_fa, cat_title)
+                if cat.exists():
+                    try:
+                        curr_cat_item = pywikibot.ItemPage.fromPage(cat)
+                        engcat = curr_cat_item.getSitelink('enwiki')
+                        # now we can be sure that it has a corresponding
+                        # category on english wikipedia are connected
+                        # but we don't need to keep track of them here
+                    except Exception as e:
+                        # But we need to keep local cats to exclude them from
+                        # update of fawiki page categories, since we don't know
+                        # right now if those are in the correct page or not
+                        loc_cats.append(cat.title())
+                else:
+                    print("#"*10+cat.title()+"#"*10)
+                    print('Fa cat doesn\'t exists')
+
+    else:
+        print("*"*10+curr_page.title()+"*"*10)
+        print('Page doesn\'t exists')
+
+    for x in eng_cats_trans:
+        if x not in loc_cats:
+            fa_cats_final.append(x)
+            print("fa_cats_final="+x)
+
+    cats_list = []
+    cats_text = ""
+
+    for x in fa_cats_final:
+        cats_list.append(x)
+
+    for x in loc_cats:
+        cats_list.append(x)
+
+    cats_list.sort()
+
+    for x in cats_list:
+        cats_text += "[[" + x + "]]\n"
+
+    old_page_txt = curr_page.text
+    new_page_text = re.sub(r"\[\[[^\[\]:]*?رده:[^\[\]]*?\]\]", "", old_page_txt, flags=re.I)
+    new_page_text = re.sub(r"\n\s*\n$", "", old_page_txt, flags=re.M)
+    new_page_text += cats_text
+
+    # out = open("out.txt", "a")
+    # out.write(new_page_text)
+    # out.close()
+    
+    curr_page.put(new_page, "بروزرسانی آزمایشی رده‌ها")
